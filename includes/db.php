15022a69d851b53c17482341b528c404cef2585d
Toby <513945442@qq.com>
2017-11-01 01:04:13 +0800

    new project

---
+++ includes/db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tobymao
+ * Date: 01/11/2017
+ * Time: 12:29 AM
+ */
---
+++ phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tobymao
+ * Date: 01/11/2017
+ * Time: 12:29 AM
+ */
+phpinfo();
+?>
