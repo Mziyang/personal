ad6f57e5d0f662921d2cf411bc9f5109133eefc6
Toby <513945442@qq.com>
2017-11-01 22:26:19 +0800

    update

--- protal.php
+++ protal.php
@@ -10,3 +10,16 @@
 //list documents download links,
 //list all function links
 ?>
+
+<ul>
+    <li><a href="#">link</a></li>
+    <li><a href="#">link</a></li>
+    <li><a href="#">link</a></li>
+
+</ul>
+<form action="#" method="post">
+    <input name="user" type="text" placeholder="user">
+    <input name="pwd" type="password" placeholder="password">
+<!--show/hide the password change the type(password/text)-->
+    <input type="reset"><input type="submit">
+</form>
