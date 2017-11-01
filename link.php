8b0cea7e4e8b0312ba97b27ef4d47b8694c398d5
Toby <513945442@qq.com>
2017-11-01 11:08:54 +0800

    update

--- link.php
+++ link.php
@@ -5,3 +5,28 @@
  * Date: 01/11/2017
  * Time: 9:50 AM
  */
+?>
+<h1>Powered by <a href="http://www.staffs.ac.uk">Staffordshire University</a></h1>
+<h2>Log in to lynda.com</h2>
+<p>Access <a href="https://shib.lynda.com/Shibboleth.sso/InCommon?providerId=https://shibboleth.staffs.ac.uk/idp/shibboleth&target=https://shib.lynda.com/InCommon">lynda.com</a></p>
+<h2>Library resources</h2>
+<p>Go to <a href="http://libguides.staffs.ac.uk/libraryresources">library</a></p>
+
+<h1>Frequent Locations</h1>
+<h2>PHP</h2>
+<p><a href="http://php.net/">PHP: Hypertext Preprocessor</a></p>
+<h2>jQuery</h2>
+<p><a href="https://jquery.org/">jQuery Foundation</a></p>
+<h2>Laravel</h2>
+<p><a href="https://laravel.com/">Laravel - The PHP Framework For Web Artisans</a></p>
+<h2>Chart.js</h2>
+<p><a href="http://www.chartjs.org/">Chart.js | Open source HTML5 Charts for your website</a></p>
+<h2>GitHub</h2>
+<p><a href="https://github.com">GitHub</a></p>
+<h2>Font Awesome</h2>
+<p><a href="http://fontawesome.io/">Font Awesome, the iconic font and CSS toolkit</a></p>
+
+
+<h1>FeedBack</h1>
+<p>If you have any comments or problems please let me know. </p>
+<p>Contact me at <a href="mailto:513945442@qq.com">513945442@qq.com</a></p>
